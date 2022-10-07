1c4412ba8938ab39ca94471f886ee0fca882c98c
JAIN Tapan <tapan.jain@soprasteria.com>
2022-10-07 12:57:54 +0530

    contract deployment 1

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.9",
+  networks: {
+    rinkeby: {
+      url: `${process.env.POKT_RINKEBY_URL}`,
+      accounts: [`${process.env.RINKEBY_PRIVATE_KEY}`],
+      gas: 2100000,
+      gasPrice: 8000000000,
+    }
+  }
 };
